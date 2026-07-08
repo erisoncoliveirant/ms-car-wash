f2f54240c0af1dcc0a2b80334cb755bcff78f0e9
Erison Neto <erisoncoliveirant@gmail.com>
2026-07-07 22:16:41 -0400

    feat: project cw-worker created

---
+++ cw-worker/src/main/java/com/car_wash/cw_worker/CwWorkerApplication.java
@@ -0,0 +1,13 @@
+package com.car_wash.cw_worker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CwWorkerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CwWorkerApplication.class, args);
+	}
+
+}
